d38ca9a557b192e40cfb2f940b2f10c8a13d110e
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2024-10-26 21:54:06 -0400

    implement abstract base class

--- Start/Ch 2/abstract_start.py
+++ Start/Ch 2/abstract_start.py
@@ -1,11 +1,19 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using Abstract Base Classes to enforce class constraints
+"""
+1. We don't want consumers of the base class to be able to create an instance of the base class itself.
 
+2. Enfoce the constraint that methods in the base class has to be implemented in the subclasses.
+    - In this example, we want to prevent GraphicShape to be instantiated.
+"""
 
-class GraphicShape:
+from abc import ABC, abstractmethod 
+
+class GraphicShape(ABC):
     def __init__(self):
         super().__init__()
 
+    @abstractmethod 
     def calcArea(self):
         pass
 
@@ -13,14 +21,19 @@ class GraphicShape:
 class Circle(GraphicShape):
     def __init__(self, radius):
         self.radius = radius
+    
+    def calcArea(self):
+        return 3.14 * (self.radius ** 2) 
 
 
 class Square(GraphicShape):
     def __init__(self, side):
         self.side = side
 
+    def calcArea(self):
+        return (self.side ** 2)
 
-g = GraphicShape()
+# g = GraphicShape()
 
 c = Circle(10)
 print(c.calcArea())
